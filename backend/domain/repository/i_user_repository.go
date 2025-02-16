5de75921a6f6bb6a3a9e1d19f3bf1b6a3672276f
saneyoshi <saneyoshi@collabos.com>
2025-02-16 21:36:55 +0900

    add:ドメインロジックのリポジトリを実装。

---
+++ backend/domain/repository/i_user_repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/akira-saneyoshi/task-app/domain/object/entity"
+)
+
+type IUserRepository interface {
+	FindUserByID(ctx context.Context, id string) (*entity.User, error)
+	FindUserByEmail(ctx context.Context, email string) (*entity.User, error)
+}
