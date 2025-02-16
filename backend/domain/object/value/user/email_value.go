a5fea5b1ca4134f960aa7c9f65e5cec774d7a45b
saneyoshi <saneyoshi@collabos.com>
2025-02-16 13:04:48 +0900

    add:ドメインロジックの値検証コードを追加。

---
+++ backend/domain/object/value/user/email_value.go
@@ -0,0 +1,24 @@
+package value
+
+import (
+	"github.com/akira-saneyoshi/task-app/domain"
+)
+
+type Email struct {
+	value string
+}
+
+func NewEmail(value string) *Email {
+	return &Email{value}
+}
+
+func (e *Email) Value() string {
+	return e.value
+}
+
+func (e *Email) Validate() error {
+	if e.value == "" {
+		return &domain.ErrValidationFailed{Msg: "email is empty"}
+	}
+	return nil
+}
