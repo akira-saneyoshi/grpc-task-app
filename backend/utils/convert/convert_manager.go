d5fe42b8888fcf92da38fa37276a91803e4102c0
saneyoshi <saneyoshi@collabos.com>
2025-02-18 00:27:09 +0900

    add:infra-sqlc add.

---
+++ backend/utils/convert/convert_manager.go
@@ -0,0 +1,64 @@
+package convert
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/akira-saneyoshi/task-app/domain/object/entity"
+	"github.com/akira-saneyoshi/task-app/infrastructure/persistence/model/db"
+)
+
+func ConvertNullString(ns sql.NullString) *string {
+	if ns.Valid {
+		return &ns.String
+	}
+	return nil
+}
+
+func ConvertNullTime(nt sql.NullTime) time.Time {
+	if nt.Valid {
+		return nt.Time
+	}
+	return time.Time{}
+}
+
+func ConvertNullTimePtr(nt sql.NullTime) *time.Time {
+	if nt.Valid {
+		return &nt.Time
+	}
+	return nil
+}
+
+func ConvertNullTimeValue(nt sql.NullTime) time.Time {
+	if nt.Valid {
+		return nt.Time
+	}
+	return time.Time{}
+}
+
+func ConvertNullStatus(ns db.NullTasksStatus) entity.Status {
+	if ns.Valid {
+		return entity.Status(ns.TasksStatus)
+	}
+	return entity.StatusPending
+}
+
+func NewSQLNullString(s *string) sql.NullString {
+	if s != nil {
+		return sql.NullString{
+			String: *s,
+			Valid:  true,
+		}
+	} else {
+		return sql.NullString{
+			Valid: false,
+		}
+	}
+}
+
+func NewSQLNullTime(t time.Time) sql.NullTime {
+	return sql.NullTime{
+		Time:  t,
+		Valid: !t.IsZero(),
+	}
+}
