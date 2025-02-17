081a9654ab6cf843f3c2e87af2ea9371f9bfec39
saneyoshi <saneyoshi@collabos.com>
2025-02-17 22:41:29 +0900

    add:domain-repository add.

---
+++ backend/domain/repository/i_task_repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/akira-saneyoshi/task-app/domain/object/entity"
+)
+
+type ITaskRepository interface {
+	FindTaskByID(ctx context.Context, id string) (*entity.Task, error)
+	FindTasksByUserID(ctx context.Context, userID string) ([]*entity.Task, error)
+	CreateTask(ctx context.Context, arg *entity.Task) (string, error)
+	UpdateTask(ctx context.Context, arg *entity.Task) error
+	DeleteTask(ctx context.Context, id string) error
+}
