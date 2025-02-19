62f9c70f6100fd50ee94db3d126e50bc680d3306
saneyoshi <saneyoshi@collabos.com>
2025-02-19 09:01:25 +0900

    fix:sqlc生成用のSQLコードを修正

--- backend/migrations/query/query_users.sql
+++ backend/migrations/query/query_users.sql
@@ -1,11 +1,11 @@
 -- name: FindUserByID :one
-SELECT id, name, email, password, is_active, created_at, updated_at
+SELECT id, name, email, password, created_at, updated_at
 FROM users
 WHERE id = ?
 LIMIT 1;
 
 -- name: FindUserByEmail :one
-SELECT id, name, email, password, is_active, created_at, updated_at
+SELECT id, name, email, password, created_at, updated_at
 FROM users
 WHERE email = ?
 LIMIT 1;
